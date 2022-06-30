06eaab653d93fd69afa827b014e3d578dfb97b7a
unknown <prashanthdilshaan@gmail.com>
2022-06-30 18:47:45 -0400

    js complete

--- utils.js
+++ utils.js
@@ -1,8 +1,20 @@
 //getting a random dice number
-function getDiceRollArray(diceCount) {
-    return new Array(diceCount).fill(0).map(function (num) {
-        return (Math.round(Math.random() * 5) + 1);
-    })
-}
 
-export { getDiceRollArray }
+function getDiceRollArray(diceCount) {
+    return new Array(diceCount).fill(0).map((num) =>
+        Math.round(Math.random() * 5) + 1);
+};
+
+function getDicePlaceholderHtml(diceCount) {
+    return new Array(diceCount).fill(0).map((num) =>
+        `<div class="placeholder-dice"></div>`
+    ).join('');
+};
+
+const getPercentage = (remainingHealth, maximumHealth) =>
+    ((100 * remainingHealth) / maximumHealth);
+
+
+
+
+export { getDiceRollArray, getDicePlaceholderHtml, getPercentage };
